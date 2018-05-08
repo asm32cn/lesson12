ed5becf53ebcb90e3c29e528d4f1c26df4c394ce
asm32 <asm32@qq.com>
2018-05-08 09:15:58 +0800

    StdAfx.cpp ... add

---
+++ App_Data/lesson12_sqlite3_db_mfc/StdAfx.cpp
@@ -0,0 +1,8 @@
+// stdafx.cpp : source file that includes just the standard includes
+//	lesson12_sqlite3_db_mfc.pch will be the pre-compiled header
+//	stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+
+
