6e4d480ba8965b86eda4ea89a560a9cec8592707
asm32 <asm32@qq.com>
2018-05-08 08:50:17 +0800

    PaLesson12JTable__jar.bat ... add

---
+++ App_Data/lesson12_sqlite3_db_java.java/PaLesson12JTable__jar.bat
@@ -0,0 +1,30 @@
+@echo off
+
+set ProjName=PaLesson12JTable
+echo Begin ...
+echo --------------------
+md classes
+ren md classes\res
+javac -d classes %ProjName%.java
+echo --------------------
+echo Done.
+
+if exist %ProjName%.manifest.txt del /f %ProjName%.manifest.txt
+
+echo Manifest-Version: 1.0> %ProjName%.manifest.txt
+echo Created-By: 1.4.2_12 (Sun Microsystems Inc.)>> %ProjName%.manifest.txt
+echo Class-Path: lib/sqlite-jdbc-3.7.2.jar>> %ProjName%.manifest.txt
+echo Main-Class: %ProjName%>> %ProjName%.manifest.txt
+
+echo Release ...
+echo --------------------
+md Release
+cd classes
+rem jar cvfm ..\Release\%ProjName%.jar ..\%ProjName%.manifest.txt %ProjName%.class lib/
+jar cvfm ..\Release\%ProjName%.jar ..\%ProjName%.manifest.txt %ProjName%.class
+cd ..
+del %ProjName%.manifest.txt
+echo --------------------
+echo Done.
+
+pause
